e0b6779423c1d7dcdef2d1c3adfe8e47718d05e8
LordJaph <dedeyolu@hotmail.com>
2023-09-13 07:05:06 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: always 0
+ */
+int main(void)
+{
+	int number;
+
+	char alphabet;
+
+	for (number = 0; number < 10; number++)
+		putchar(number + '0');
+	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+	putchar(alphabet);
+	return (0);
+}
